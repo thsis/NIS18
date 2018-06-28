c491c29c4ff6a0e22949516a7bb77fc2ee637191
Thomas Siskos <thomas.siskos91@gmail.com>
2018-06-28 10:16:35 +0200

    Define class for unit-tests.

--- tests/tests_eigen.py
+++ tests/tests_eigen.py
@@ -3,7 +3,7 @@ Automated tests for different algorithms.
 """
 import os
 import numpy as np
-import threading
+from threading import Thread
 import pandas as pd
 from algorithms import eigen
 from scipy.stats import ortho_group
@@ -12,101 +12,61 @@ from tqdm import trange, tqdm
 data_out = os.path.join("data", "accuracy_tests.csv")
 
 
-def get_test_matrix(dim):
-    """Return matrix with assosiated Eigenvalues."""
-    eigenvalues = np.random.uniform(size=dim)
-    eigenvectors = ortho_group.rvs(dim=dim)
-    Lambda = np.diag(eigenvalues)
+class AlgoTest(object):
+    def __init__(self, algo, dim, n_tests=1000, jobs=1, *args, **kwargs):
+        assert n_tests % jobs == 0
+        self.algorithm = self.__get_test_algorithm(algo, *args, **kwargs)
+        self.dim = dim
+        self.n = n_tests // jobs
+        self.failed = []
+        self.jobs = jobs
+        self.result = None
 
-    matrix = np.dot(eigenvectors, Lambda).dot(eigenvectors.T)
+    def __get_test_algorithm(self, algorithm, *args, **kwargs):
+        def algo(X):
+            return algorithm(X, *args, **kwargs)
+        return algo
 
-    order = np.abs(eigenvalues).argsort()[::-1]
-    return matrix, eigenvalues[order]
+    def __get_test_matrix(self):
+        """Return matrix with assosiated Eigenvalues."""
+        eigenvalues = np.random.uniform(size=self.dim)
+        eigenvectors = ortho_group.rvs(dim=self.dim)
+        Lambda = np.diag(eigenvalues)
 
+        matrix = np.dot(eigenvectors, Lambda).dot(eigenvectors.T)
 
-def test_algo(algo, Ntests=1000, dim=3, *args, **kwargs):
-    """
-    Test routine that allows for threading. Note that the variables:
-    failed, critical and problematic need to be defined in the enveloping or
-    global scope beforehand.
+        order = np.abs(eigenvalues).argsort()[::-1]
+        return matrix, eigenvalues[order]
 
-    Parameters:
-        - algo: algorithm to be tested
-        - Ntests: number of tests to compute
-        - dim: dimensions of matrix
-        - *args, **kwargs: additional arguments to be passed to algo.
+    def __run_test(self):
+        """Run singular test."""
+        mat, true_eig = self.__get_test_matrix()
+        test_eig, _ = self.algorithm(mat)
+        test_res = np.alltrue(np.isclose(true_eig, test_eig))
+        self.failed.append(not test_res)
 
-    Returns:
-        - None, but will update the variables failed, critical and problematic.
-            + failed: number of failed tests
-            + critical: number of ZeroDivisionErrors
-            + problematic: list of numpy arrays which led to wrong eigenvalues.
-    """
-    global failed
-    global critical
-    global problematic
+    def __run_tests(self):
+        """Run multiple tests."""
+        for _ in range(self.n):
+            self.__run_test()
 
-    for _ in range(Ntests):
-        try:
-            A, true_eig = get_test_matrix(dim=dim)
-            my_eig, _ = algo(A, *args, **kwargs)
-            assert np.alltrue(np.isclose(my_eig, true_eig))
+    def run(self):
+        """Distribute tests accross threads."""
 
-        except AssertionError:
-            failed += 1
-            problematic.append(A)
+        threadlist = [None] * self.jobs
 
-        except ZeroDivisionError:
-            critical += 1
+        for i in range(self.jobs):
+            threadlist[i] = Thread(target=self.__run_tests)
+            threadlist[i].start()
 
+        for thread in threadlist:
+            thread.join()
 
-def threaded_tests(algo, N, nWorkers=10, verbose=True, *args, **kwargs):
-    global failed
-    global critical
-    global problematic
-
-    assert N % nWorkers == 0
-
-    n = N // nWorkers
-    threadlist = [None] * nWorkers
-
-    for i in range(nWorkers):
-        threadlist[i] = threading.Thread(target=test_algo,
-                                         args=(algo, n, *args))
-        threadlist[i].start()
-
-    for i in range(nWorkers):
-        threadlist[i].join()
-
-    logstr = """
-    {} out of {} tests failed.
-    {} tests failed critically.
-    """.format(failed, N, critical)
-
-    if verbose:
-        print(logstr)
+        self.result = sum(self.failed)
 
 
-# Tests
-results = {
-    "algorithm": [],
-    "dimension": [],
-    "maxiter": [],
-    "failed": []}
-
-for algo in tqdm([eigen.jacobi, eigen.qrm, eigen.qrm2, eigen.qrm3]):
-    for dim in trange(3, 15):
-        for maxiter in 1000, 10000, 100000:
-            if algo.__name__ == eigen.jacobi:
-                maxiter = 1e-6
-            failed = 0
-            critical = 0
-            problematic = []
-            threaded_tests(algo, 1000, 20, False, dim, maxiter)
-            results["algorithm"].append(algo.__name__)
-            results["dimension"].append(dim)
-            results["maxiter"].append(maxiter)
-            results["failed"].append(failed)
-
-test_data = pd.DataFrame(results)
-test_data.to_csv(data_out)
+# Unit tests
+algotest = AlgoTest(algo=eigen.qrm2, dim=3, n_tests=1000, jobs=10, maxiter=20)
+algotest.run()
+algotest.failed
+algotest.result
