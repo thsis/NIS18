a3c20a5a2ed1f3979a658981962ba98ed8be0dec
Thomas Siskos <thomas.siskos91@gmail.com>
2018-06-29 14:12:27 +0200

    Refine tests

--- tests/tests_eigen.py
+++ tests/tests_eigen.py
@@ -2,18 +2,27 @@
 Automated tests for different algorithms.
 """
 import os
+import sys
 import numpy as np
 from threading import Thread
 import pandas as pd
 from algorithms import eigen
 from scipy.stats import ortho_group
-from tqdm import trange
+from tqdm import tqdm
+from functools import wraps
+
 
 data_out = os.path.join("data", "accuracy_tests.csv")
 
 
 class AlgoTest(object):
-    def __init__(self, algo, dim, n_tests=1000, jobs=1, *args, **kwargs):
+    tests = {"algorithm": [],
+             "dimension": [],
+             "maxiter": [],
+             "failed": []}
+
+    def __init__(self, algo, dim, filepath, n_tests=1000, jobs=1,
+                 *args, **kwargs):
         assert n_tests % jobs == 0
         self.algorithm = self.__get_test_algorithm(algo, *args, **kwargs)
         self.dim = dim
@@ -21,8 +30,14 @@ class AlgoTest(object):
         self.failed = []
         self.jobs = jobs
         self.result = None
+        self.path = filepath
+        self.maxiter = kwargs.get("maxiter", None)
+
+        if not os.path.exists(self.path):
+            self.save(header=["algorithm", "dimension", "maxiter", "failed"])
 
     def __get_test_algorithm(self, algorithm, *args, **kwargs):
+        @wraps(algorithm)
         def algo(X):
             return algorithm(X, *args, **kwargs)
         return algo
@@ -48,7 +63,11 @@ class AlgoTest(object):
     def __run_tests(self):
         """Run multiple tests."""
         for _ in range(self.n):
-            self.__run_test()
+            try:
+                self.__run_test()
+            except (KeyboardInterrupt, SystemExit):
+                self.__save()
+                sys.exit(0)
 
     def run(self):
         """Distribute tests accross threads."""
@@ -56,46 +75,75 @@ class AlgoTest(object):
         threadlist = [None] * self.jobs
 
         for i in range(self.jobs):
-            threadlist[i] = Thread(target=self.__run_tests)
+            threadlist[i] = Thread(target=self.__run_tests, daemon=True)
             threadlist[i].start()
 
         for thread in threadlist:
             thread.join()
 
         self.result = sum(self.failed)
+        self.tests["algorithm"].append(self.algorithm.__name__)
+        self.tests["dimension"].append(self.dim)
+        self.tests["failed"].append(self.result)
+        if self.algorithm.__name__ == "jacobi":
+            self.tests["maxiter"].append(None)
+        else:
+            self.tests["maxiter"].append(self.maxiter)
+
+    def save(self, header=False):
+        if os.path.exists(self.path):
+                print("Saving results.")
+        df = pd.DataFrame(self.tests)
+        with open(self.path, 'a') as f:
+            df.to_csv(f, header=header, index=False,
+                      columns=["algorithm", "dimension", "maxiter", "failed"])
 
 
 # Unit tests
-tests = {"algorithm": [],
-         "dimension": [],
-         "maxiter": [],
-         "failed": []}
+if __name__ == "__main__":
+    # Define Flags.
+    JOBS = 20
+    MAXITER = (10, 100, 1000, 10000)
+    DIMS = range(3, 8)
+    ALGOS = {'jacobi': eigen.jacobi,
+             'qrm': eigen.qrm,
+             'qrm2': eigen.qrm2,
+             'qrm3': eigen.qrm3}
 
-# Jacobi-Method
-for dim in trange(3, 11):
-    algo_test = AlgoTest(algo=eigen.jacobi, dim=dim, jobs=20)
+    # Define parameters of all runs.
+    parameters = []
+    for algo in ALGOS.values():
+        for maxiter in MAXITER:
+            for dim in range(3, 8):
+                param = (algo,
+                         maxiter if algo.__name__ != "jacobi" else None,
+                         dim)
+                parameters.append(param)
 
-    algo_test.run()
-    tests["algorithm"].append(eigen.jacobi.__name__)
-    tests["dimension"].append(dim)
-    tests["maxiter"].append(None)
-    tests["failed"].append(algo_test.result)
+    # Check progress of previous runs.
+    if os.path.exists(data_out):
+        required = [(algo.__name__, m, dim) for (algo, m, dim) in parameters]
+        required = set(required)
 
+        progress = pd.read_csv(data_out)
+        done = []
+        for (a, d, m, _) in progress.values:
+            if np.isnan(m):
+                param = (a, d, None)
+            else:
+                param = (a, d, m)
+            done.append(param)
+        done = set(done)
 
-# QR-Method
-for algo in eigen.qrm, eigen.qrm2, eigen.qrm3:
-    print("Start tests for", algo.__name__)
-    for maxiter in 100, 1000, 10000:
-        for dim in trange(3, 11):
-            algo_test = AlgoTest(algo=algo,
-                                 dim=dim,
-                                 maxiter=maxiter,
-                                 jobs=20)
-            algo_test.run()
-            tests["algorithm"].append(algo.__name__)
-            tests["dimension"].append(dim)
-            tests["maxiter"].append(maxiter)
-            tests["failed"].append(algo_test.result)
+        to_do = required.difference(done)
+        parameters = [(ALGOS[a], m, d) for a, m, d in to_do]
 
+    for algo, maxiter, dim in tqdm(parameters):
+                algo_test = AlgoTest(filepath=data_out,
+                                     algo=algo,
+                                     dim=dim,
+                                     maxiter=maxiter,
+                                     jobs=JOBS)
+                algo_test.run()
 
-tests
+    algo_test.save()
